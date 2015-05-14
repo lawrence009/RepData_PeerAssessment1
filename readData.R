d18e167e074b8e2717d3f0b4e7848013310eeb9a
Lawrence Chen <lawrence.chen@experian.com>
2015-05-13 17:08:47 -0700

    first pass of complete solution

--- readData.R
+++ readData.R
@@ -13,9 +13,9 @@ str(subject)
 
 total.steps.day <- tapply(subject$steps, subject$date, sum, na.rm = T)
 
-mean.steps.interval <- tapply(subject$steps, subject$interval, mean, na.rm = T)
+avg.steps.interval <- tapply(subject$steps, subject$interval, mean, na.rm = T)
 
-length(which(is.na(subject$steps)))
+print(sum(is.na(subject$steps)))
 
 # 0=Sunday, 6=Saturday
 is.weekend <- function(wday) {
@@ -26,4 +26,17 @@ is.weekend <- function(wday) {
     }
 }
 
-wknd <- sapply(subject$datetime$wday, is.weekend)
+subject$wknd <- sapply(subject$datetime$wday, is.weekend)
+
+
+
+t.subject <- subject
+t.subject$steps[(which(is.na(t.subject)))] <- avg.steps.interval
+t.subject$wknd <- factor(t.subject$wknd, levels = c(T, F), labels = c('weekend', 'weekday'))
+
+df <- aggregate(t.subject$steps, by = list(t.subject$interval, t.subject$wknd), mean)
+colnames(df) <- c('interval', 'wkd', 'avg.steps')
+df$time <- seq(from = 0, by = 5, length.out = 288)
+xyplot(avg.steps ~ time | wkd, data = df, type = 'l', layout = c(1, 2))
+
+
